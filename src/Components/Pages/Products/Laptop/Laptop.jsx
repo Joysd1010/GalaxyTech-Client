c29f8c3cc4d5f924a1a3ce7e997d112408b7aef7
Joy Sutradhar <joysutradharaj@gmail.com>
2024-02-02 19:00:14 +0600

    Laptop feild is complete with functionality

--- src/Components/Pages/Products/Laptop/Laptop.jsx
+++ src/Components/Pages/Products/Laptop/Laptop.jsx
@@ -18,8 +18,6 @@ const Laptop = () => {
   const indexOfFirstCard = (currentPage) => (currentPage - 1) * postPerPage;
   const indexOfLastCard = (currentPage) => currentPage * postPerPage;
 
- 
-
   const setCurrentPost = () => {
     const start = indexOfFirstCard(CurrentPage);
     const end = indexOfLastCard(CurrentPage);
@@ -33,13 +31,13 @@ const Laptop = () => {
 
   const generatePageNumbers = (totalPages, currentPage) => {
     const pageNumbers = [];
-  
+
     if (currentPage > 1) {
       pageNumbers.push(
         <button
           key="prev"
           onClick={() => handlePageChange(currentPage - 1)}
-          className="prev-btn"
+          className="prev-btn underline hover:text-red-600"
           disabled={currentPage === 1}
         >
           Previous
@@ -47,34 +45,35 @@ const Laptop = () => {
       );
     } else {
       pageNumbers.push(
-        <button
-          key="prev"
-          className="prev-btn btn-disabled text-gray-400"
-        >
+        <button key="prev" className="prev-btn btn-disabled text-gray-400">
           Previous
         </button>
       );
     }
-  
+
     for (let i = 1; i <= totalPages; i++) {
       pageNumbers.push(
         <button
           key={i}
           onClick={() => handlePageChange(i)}
-          className={i === currentPage ? "active text-white px-3 bg-red-600 rounded-md" : ""}
+          className={
+            i === currentPage
+              ? "active text-white px-3 bg-red-600 rounded-md"
+              : ""
+          }
           disabled={i === currentPage}
         >
           {i}
         </button>
       );
     }
-  
+
     if (currentPage < totalPages) {
       pageNumbers.push(
         <button
           key="next"
           onClick={() => handlePageChange(currentPage + 1)}
-          className="next-btn"
+          className="next-btn underline hover:text-red-600"
           disabled={currentPage === totalPages}
         >
           Next
@@ -82,15 +81,12 @@ const Laptop = () => {
       );
     } else {
       pageNumbers.push(
-        <button
-          key="next"
-          className="next-btn btn-disabled text-gray-400"
-        >
+        <button key="next" className="next-btn btn-disabled text-gray-400">
           Next
         </button>
       );
     }
-  
+
     return pageNumbers;
   };
   const getAllLaptop = async () => {
@@ -98,7 +94,7 @@ const Laptop = () => {
       const response = await axiosPoint.get("/laptop");
       console.log(response.data);
       setLaptop(response.data);
-      setUseLaptop(response.data.slice(0,12) );
+      setUseLaptop(response.data.slice(0, 12));
       setMinPrice(
         Math.min(
           ...response.data.map((item) => item.keyFeatures.discountedPrice)
@@ -134,6 +130,60 @@ const Laptop = () => {
       console.error("Error fetching laptop data:", error);
     }
   };
+  //------------------------Price filter-----------------
+  const handleOnchange = (value) => {
+    setRange(value);
+    setUseLaptop(
+      AllLaptop.filter((item) => item.keyFeatures.discountedPrice <= value)
+    );
+  };
+  //-------------------------UserDefinedFilter--------------------
+  const HandleChoice = (container, attribute, value) => {
+    console.log(
+      AllLaptop.filter((item) => item[container][attribute] == value)
+    );
+    setUseLaptop(
+      AllLaptop.filter((item) => item[container][attribute] == value)
+    );
+  };
+  //-------------------------Shorting------------------------------------
+  const shortingDesc = (laptop) => {
+    const filter =  laptop.sort(
+      (a, b) => b.keyFeatures.discountedPrice - a.keyFeatures.discountedPrice
+    ); 
+    console.log(filter[0].keyFeatures.discountedPrice)
+    return filter;
+  };
+ 
+ 
+
+  const shortingAsc = (laptop) => {
+    const filterLaptop = laptop.sort(
+      (a, b) => a.keyFeatures.discountedPrice - b.keyFeatures.discountedPrice
+    );
+    console.log(filterLaptop[0].keyFeatures.discountedPrice);
+    return filterLaptop;
+  };
+
+  const handleSortByPrice = (event) => {
+    const sortBy = parseInt(event.target.value);
+
+    switch (sortBy) {
+      case 2:
+        const sortedByPrice = shortingAsc(UseLaptop);
+        setUseLaptop(sortedByPrice);
+        console.log(UseLaptop)
+        break;
+      case 3:
+        const sortedByPriceDescending = shortingDesc(UseLaptop);
+        setUseLaptop(sortedByPriceDescending);
+        console.log(UseLaptop)
+        break;
+      default:
+        break;
+    }
+  };
+  //-----------------------fetching data-------------------------
   useEffect(() => {
     if (param === "All") {
       getAllLaptop();
@@ -142,56 +192,162 @@ const Laptop = () => {
     }
     setCurrentPost();
   }, [param]);
-  return (
-    <div className="bg-slate-100 px-20 py-5 grid grid-cols-4 gap-5">
-      <div>
-        {/*-------------------------- Price range adjuster-------------------------------- */}
-        <div>
-          <div className=" bg-white rounded-md">
-            <h1 className=" text-lg font-medium py-2 px-5 "> Price Range</h1>
-            <hr />
-            <div className="px-5 py-3">
-              <div className=" flex justify-between px-2">
-                <p>{MinPrice}$</p>
-                <p>{MaxPrice}$</p>
-                {console.log(MinPrice, MaxPrice)}
-              </div>
-              <input
-                onChange={(event) => handleOnchange(event.target.value)}
-                type="range"
-                min={MinPrice}
-                max={MaxPrice}
-                defaultValue={MaxPrice}
-                // value={Range}
-                className="range border-2 bg-white range-error "
-              />
-            </div>
 
-            <div className="px-5 pb-2 text-center">
-              <h1> Under</h1>
-              <hr className="mx-2/3" />
-              <h1>{Range} $</h1>
+  
+  return (
+    <div className="bg-indigo-100 px-10 py-5 grid grid-cols-4 gap-5">
+      <div className=" flex flex-col gap-2">
+        {/*-------------------------- Price range adjuster-------------------------------- Intel*/}
+
+        <div className=" bg-white rounded-md">
+          <h1 className=" text-lg font-medium py-2 px-5 "> Price Range</h1>
+          <hr />
+          <div className="px-5 py-3">
+            <div className=" flex justify-between px-2">
+              <p>{MinPrice}$</p>
+              <p>{MaxPrice}$</p>
             </div>
+            <input
+              onChange={(event) => handleOnchange(event.target.value)}
+              type="range"
+              min={MinPrice}
+              max={MaxPrice}
+              defaultValue={MaxPrice}
+              className="range border-2 bg-white range-error "
+            />
+          </div>
+
+          <div className="px-5 pb-2 text-center">
+            <h1> Under</h1>
+            <hr className="mx-2/3" />
+            <h1>{Range} $</h1>
           </div>
         </div>
+
         {/*-----------------------------Processor Filter---------------------------------- */}
-        <div></div>
+        <div className=" bg-white rounded-md ">
+          <h1 className=" text-lg font-medium py-2 px-5 "> Processor</h1>
+          <hr />
+          <div className=" px-5 py-2 class-name flex gap-2">
+            <button
+              onClick={() => HandleChoice("processor", "brand", "Intel")}
+              className=" px-3 bg-blue-700 text-white btn btn-active hover:bg-blue-700 "
+            >
+              Intel
+            </button>
+            <button
+              onClick={() => HandleChoice("processor", "brand", "AMD")}
+              className=" px-3 bg-red-600 text-white btn btn-active hover:bg-red-600 "
+            >
+              AMD
+            </button>
+          </div>
+        </div>
+        {/* ------------------------------Processor core------------------------ */}
+        <div className=" bg-white rounded-md ">
+          <h1 className=" text-lg font-medium py-2 px-5 "> Number of Core</h1>
+          <hr />
+          <div className="px-5 py-2 flex flex-col gap-2">
+            <label
+              className="flex hover:cursor-pointer gap-2 hover:bg-indigo-50 p-1 rounded-sm"
+              htmlFor="core1"
+            >
+              <input
+                type="checkbox"
+                onChange={() => HandleChoice("processor", "core", 4)}
+                className="w-5"
+                value=""
+                id="core1"
+              />{" "}
+              <p> 4</p>
+            </label>
+            <label
+              className="flex hover:cursor-pointer gap-2 hover:bg-indigo-50 p-1 rounded-sm"
+              htmlFor="core2"
+            >
+              <input
+                type="checkbox"
+                onChange={() => HandleChoice("processor", "core", 6)}
+                className="w-5"
+                value=""
+                id="core2"
+              />{" "}
+              <p> 6</p>
+            </label>
+            <label
+              className="flex hover:cursor-pointer gap-2 hover:bg-indigo-50 p-1 rounded-sm"
+              htmlFor="core3"
+            >
+              <input
+                type="checkbox"
+                onChange={() => HandleChoice("processor", "core", 8)}
+                className="w-5"
+                value=""
+                id="core3"
+              />{" "}
+              <p> 8</p>
+            </label>
+          </div>
+        </div>
+
+        {/*-----------------------------graphics---------------------------------- */}
+        <div className=" bg-white rounded-md ">
+          <h1 className=" text-lg font-medium py-2 px-5 "> Graphics</h1>
+          <hr />
+          <div className=" px-5 py-2 class-name flex  gap-2">
+            <button
+              onClick={() => HandleChoice("graphics", "brand", "Intel")}
+              className=" px-2 bg-blue-700 text-white btn btn-active hover:bg-blue-700 "
+            >
+              Intel
+            </button>
+            <button
+              onClick={() => HandleChoice("graphics", "brand", "AMD")}
+              className=" px-2 bg-red-600 text-white btn btn-active hover:bg-red-600 "
+            >
+              AMD Radeon
+            </button>
+            <button
+              onClick={() => HandleChoice("graphics", "brand", "Nvidia")}
+              className=" px-2 bg-[#73B301] text-white btn btn-active hover:bg-[#73B301] "
+            >
+              NVIDIA
+            </button>
+          </div>
+        </div>
       </div>
 
       <div className=" col-span-3 ">
-        <div className=" p-3 bg-white font-semibold text-lg  rounded-md">
-          <h1>{param.toUpperCase()}</h1>
+        <div className=" px-5 py-3 bg-white items-center  flex justify-between  rounded-md">
+          <h1 className="font-semibold text-lg">{param.toUpperCase()}</h1>
+          <div className=" flex items-center gap-2">
+            <p className=" text-gray-500">Sort By : </p>{" "}
+            <div>
+              <select
+                onChange={handleSortByPrice}
+                className="bg-slate-200 w-42 rounded-md px-3 py-2 outline-none"
+              >
+                <option value="1">Default</option>
+                <option value="2">Price (low -> high)</option>
+                <option value="3"> Price (high -> low)</option>
+              </select>
+            </div>
+          </div>
         </div>
-        <div className=" grid grid-cols-3 gap-5 pt-4 ">
-        {UseLaptop.map((item,index) => (
-        <LapTopCard key={item._id} state={item} sl={index} />
-      ))} 
+        <div className=" grid grid-cols-3 gap-5 pt-2 ">
+          {UseLaptop.length > 1 ? (
+            UseLaptop.map((item, index) => (
+              <LapTopCard key={item._id} state={item} sl={index} />
+            ))
+          ) : (
+            <h1>Sorry No Laptop Found</h1>
+          )}
         </div>
-      <div className="pagination flex gap-6 py-6">
-        {generatePageNumbers(
-          Math.ceil(AllLaptop.length / postPerPage),
-          CurrentPage
-        )}
+        <div className="pagination flex gap-6 py-6">
+          {generatePageNumbers(
+            Math.ceil(AllLaptop.length / postPerPage),
+            CurrentPage
+          )}
         </div>
       </div>
     </div>
